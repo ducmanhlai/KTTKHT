a4ddc4506b52b3e566ea1b20cf607106a2ddd628
ducmanhlai <ducmanhlai22@gmail.com>
2023-05-24 23:28:52 +0700

    create project

---
+++ frontend/src/App.js
@@ -0,0 +1,13 @@
+import './scss/test.scss';
+function App() {
+  return (
+    <div>
+      <h1 className='App'>
+        Hello world!
+      </h1>
+      <a className='underline'>hihi</a>
+    </div>
+  );
+}
+
+export default App;
