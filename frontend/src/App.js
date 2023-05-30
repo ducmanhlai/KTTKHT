60eb1460b9e81bbb8e52569dc1815baa30b7afbd
Xiao Ming <minbao4869@gmail.com>
2023-05-30 21:30:12 +0700

    Xiao Ming Test

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,11 +1,11 @@
-import './scss/test.scss';
+import "./scss/test.scss";
 function App() {
   return (
     <div>
-      <h1 className='App'>
-        Hello world!
-      </h1>
-      <a className='underline'>hihi</a>
+      <h1 className="App">Hello world!</h1>
+      <a className="underline">hihi</a>
+
+      <div className="mt-10">Xiao Ming</div>
     </div>
   );
 }
