b872e23e4b208876e4f706b2fb6ed76ba8c5fa87
ducmanhlai <ducmanhlai22@gmail.com>
2023-06-01 15:03:27 +0700

    add config

--- backend/routers/auth.js
+++ backend/routers/auth.js
@@ -2,4 +2,5 @@ import { Router } from "express";
 import authController from "../controllers/authController";
 const authRouter = Router();
 authRouter.post('/login',authController.login)
+authRouter.post('/signup',authController.signUp)
 export default authRouter
