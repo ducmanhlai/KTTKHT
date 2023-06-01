e27de7553b46d00227a23412f306ae8ef5b4f104
Phong Ne <PHONGCENA\PhongCENA>
2023-06-01 16:21:03 +0700

    test

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -1,11 +1,11 @@
-class authController{
-    async login(req,res){
-        res.send({data:1+1})
+class authController {
+    async login(req, res) {
+        res.status(200).json({ data: 1 + 1 })
     }
-    async signUp(req,res)
-    {
-         console.log(req.body)
-         res.status(401).send({data:'ok',message:'thành công',errCode:0})
+
+    async signUp(req, res) {
+        console.log(req.body)
+        res.status(200).send({ data: 'ok', message: 'thành công', errCode: 0 })
     }
 }
 export default new authController()
