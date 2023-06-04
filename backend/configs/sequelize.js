e1a7b366dd784420b0e9495a0cd94b014e6f1cad
ducmanhlai <ducmanhlai22@gmail.com>
2023-06-04 17:28:01 +0700

    à

--- backend/configs/sequelize.js
+++ backend/configs/sequelize.js
@@ -3,22 +3,6 @@ import * as dotenv from 'dotenv';
 import initModels from "../models/init-models";
 dotenv.config()
 
-<<<<<<< HEAD
-const sequelize = new Sequelize(process.env.DATABASE_NAME,process.env.LOGIN_NAME,process.env.PASSWORD, {
-  host: process.env.SERVER_NAME,
-  dialect:'mysql'
-});
-
-  (async ()=>{
-    try {
-      await sequelize.authenticate();
-      console.log('Connection has been established successfully.');
-    } catch (error) {
-      console.error('Unable to connect to the database:', error);
-    }
-  })()
- 
-=======
 const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.LOGIN_NAME, process.env.PASSWORD, {
   host: process.env.SERVER_NAME,
   dialect: 'mysql',
@@ -36,7 +20,6 @@ const sequelize = new Sequelize(process.env.DATABASE_NAME, process.env.LOGIN_NAM
   }
 })()
 
->>>>>>> 4088da37274717776f71b3ae90974fdb2e3b8ca1
 
 export default initModels(sequelize);
 export { sequelize };
