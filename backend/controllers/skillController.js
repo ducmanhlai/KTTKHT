e32972f96837e31fa9c6c362307a5d9b129cef05
ducmanhlai <ducmanhlai22@gmail.com>
2023-06-06 16:13:14 +0700

    add skill

--- backend/controllers/skillController.js
+++ backend/controllers/skillController.js
@@ -1,12 +1,48 @@
 import Models from '../configs/sequelize';
 const SkillModel = Models.skill_hero;
 class skillController {
-   async create(req,res){
-     try {
-        const listSkill = req.body.listSkill;
-     } catch (error) {
-        
-     }
+   async get(req, res) {
+      try {
+         const id_hero = req.query.id_hero;
+         const listSkill = await SkillModel.findAll({
+            where: {
+               id_hero: id_hero
+            }
+         });
+         res.send({
+            data: listSkill,
+            message: 'Lấy dữ liệu thành công'
+         })
+      } catch (err) {
+         console.log(err)
+         res.send({
+            data: [],
+            message: 'Lỗi lấy dữ liệu'
+         })
+      }
+   }
+   async create(req, res) {
+      try {
+         const { listSkill, idHero } = req.body;
+         let newlistSkill = listSkill.map(item => {
+            return {
+               ...item,
+               id_hero: idHero
+            }
+         });
+         const result = await SkillModel.bulkCreate(newlistSkill);
+         
+         res.status(200).send({
+            message: 'Tạo thành công',
+            data: result
+         })
+      } catch (error) {
+         console.log(error)
+         res.status(400).send({
+            message: 'Tạo thất bại',
+            data: [],
+         })
+      }
    }
 }
 export default new skillController()
--- backend/routers/hero.js
+++ backend/routers/hero.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import heroController from "../controllers/heroController";
 const heroRouter = Router();
 heroRouter.get('/get',heroController.get);
-heroRouter.post('/create',heroController.createHero)
-heroRouter.put('/update',heroController.updateHero)
+heroRouter.post('/create',heroController.createHero);
+heroRouter.put('/update',heroController.updateHero);
 export default heroRouter
