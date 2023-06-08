df1840d288dd92a9606d70d49d6604d8e30bf13d
ducmanhlai <ducmanhlai22@gmail.com>
2023-06-08 15:27:07 +0700

    add payment

--- backend/routers/index.js
+++ backend/routers/index.js
@@ -3,10 +3,13 @@ import heroRouter from "./hero";
 import adminRouter from "./admin";
 import skillRouter from "./skill";
 import skinRouter from "./skin";
+import paymentRouter from "./payment";
+import auth from '../middleware/authenJWT'
 export default function router(app) {
    app.use('/api/v1/auth', authRouter);
    app.use('/api/v1/hero', heroRouter);
    app.use('/api/v1/admin', adminRouter);
    app.use('/api/v1/skill', skillRouter)
-   app.use('/api/v1/skin', skinRouter)
+   app.use('/api/v1/skin', auth.authenUser,skinRouter);
+   app.use("/api/v1/payment",paymentRouter)
 }
