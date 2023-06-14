1627703135f95bc57922ef1a30d520beaf3b7cb4
Phong Ne <PHONGCENA\PhongCENA>
2023-06-14 15:14:05 +0700

    foreign key

--- backend/routers/skill.js
+++ backend/routers/skill.js
@@ -1,7 +1,8 @@
 import { Router } from "express";
 import skillController from "../controllers/skillController";
-const skillRouter= Router();
-skillRouter.post('/create',skillController.create);
-skillRouter.get('/get',skillController.get)
-skillRouter.put('/update',skillController.update)
+const skillRouter = Router();
+skillRouter.post('/create', skillController.create);
+skillRouter.get('/get', skillController.get)
+skillRouter.get('/getdetail', skillController.getDetail)
+skillRouter.put('/update', skillController.update)
 export default skillRouter
