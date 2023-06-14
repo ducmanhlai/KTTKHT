5eb6ff6c9bfd9c4749a2141539a2f22ac90204a5
Phong Ne <PHONGCENA\PhongCENA>
2023-06-14 16:20:13 +0700

    conflict

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -22,29 +22,17 @@ class userController {
         }
     }
 
-    //Khóa người dùng không nghiêm túc
-    async blockUser(req, res) {
+    //Sửa thông tin người dùng
+    async updateUser(req, res) {
         try {
-            //Cần id người dùng để lấy email
-            let id_account = req.query.id_account
-            let dataUser = await Model.account.findOne({ where: { id: id_account, id_role: 1, status: 0 } })
+            let id_account = auth.tokenData(req).id
+            let { phone, name_user, avatar, id_card } = req.body
+            console.log(phone, name_user, avatar, id_card);
 
-            //Admin soạn nội dung gửi gắm đến người chơi không nghiêm túc
-            let { content } = req.body
+            let dataUser = await Model.account.findOne({ where: { id_account }, raw: true })
 
-            if (dataUser) {
-                let email = dataUser.dataValues.email;
-                //Gửi mail đi cho người dùng bị khóa
-                mail.sendAccountBlocked(email, content)
-                //update trạng thái người dùng
-                dataUser.status = 1
-                await dataUser.save();
-                // let updateStatus = await Model.account.findOne({ where: { id: id_account }, raw: true })
-                return res.status(200).json({
-                    errCode: 0,
-                    message: 'Khóa thành công tài khoản người dùng không nghiêm túc chấp hành nội quy của trò chơi!'
-                })
-            }
+
+
             return res.status(400).json({
                 errCode: 1,
                 message: 'Khóa tài khoản người dùng thất bại!'
--- backend/routers/user.js
+++ backend/routers/user.js
@@ -2,6 +2,6 @@ import { Router } from "express";
 import userController from "../controllers/userController";
 const userRouter = Router();
 userRouter.get('/getinfo', userController.getInfoUser)
-userRouter.post('/blockuser?:id_account', userController.blockUser)
+userRouter.put('/update', userController.updateUser)
 userRouter.post('/unlockuser?:id_account', userController.unlockUser)
 export default userRouter
